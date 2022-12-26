91e548a23878a2de921348aeb657c3b76da1ae4e
OAO <36335031+mimibox543@users.noreply.github.com>
2022-12-26 23:25:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+import discord
+
+intent = discord.Intents.default()
+intent.typing = False
+intent.presences = False
+
+from discord .ext import commands
+
+bot = commands.Bot(command_prefix='!', intents=intent)
+
+@bot.event
+async def on_read():
+    print(">> Bot is awake<<")
+
+bot.run('MTA1NjkzNjEyNTM0NTg0NTM0OQ.GqRr-E.T-pnmGcsq3jMTx-1fO7miPW4eEU_eW5247GquI')
